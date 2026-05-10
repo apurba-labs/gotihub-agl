5c9b50d57636e3cae9c7d5e4ae8f4c38c7c9ee6d
Apurba Singh <aprubansinghdev@gmail.com>
2026-05-11 03:55:24 +0600

    style: optimized sidebar width and finalized blockchain explorer UI

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,6 +52,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->sidebarWidth('16rem')
+            ->sidebarCollapsibleOnDesktop();
     }
 }
