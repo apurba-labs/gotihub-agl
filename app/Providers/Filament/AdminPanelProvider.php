7830b8d17dbb0ad125d254b3c4c62c878606aec1
Apurba Singh <aprubansinghdev@gmail.com>
2026-05-14 23:00:33 +0600

    feat: customize branding for GotiHub & ApurbaLabs with demo credentials

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,9 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->favicon(asset('favicon.ico'))
             ->login()
+            ->brandName('GotiHub | ApurbaLabs')
             ->colors([
                 'primary' => Color::Amber,
             ])
